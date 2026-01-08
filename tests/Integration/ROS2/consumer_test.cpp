7a8dc508beb77000f52f6a0be5f5831279057d52
Guillaume Sarthou <gsarthou@laas.fr>
2026-01-08 16:37:10 +0100

    [CI] deploy for ROS2

---
+++ tests/Integration/ROS2/consumer_test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "hello_worlds/Engine.h"
+
+int main()
+{
+  // Try to instantiate a class that uses PhysX internally
+  hws::Engine* engine = nullptr;
+  std::cout << "Successfully linked and initialized physics!" << std::endl;
+  return 0;
+}
---
+++ tests/Integration/ROS2/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+TEST_PKG_DIR="$1/hello_worlds_consumer"
+
+mkdir -p ${TEST_PKG_DIR}
+cp ./ros2_CMakeLists.txt ${TEST_PKG_DIR}/CMakeLists.txt
+cp ./ros2_package.xml ${TEST_PKG_DIR}/package.xml
+cp ./consumer_test.cpp ${TEST_PKG_DIR}/consumer_test.cpp
