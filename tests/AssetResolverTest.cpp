3a0efb2033a306c1c3257b4471001d8b03283c17
Guillaume Sarthou <gsarthou@laas.fr>
2026-03-09 10:18:24 +0100

    [Tests] adapt test for ROS2

--- tests/AssetResolverTest.cpp
+++ tests/AssetResolverTest.cpp
@@ -6,19 +6,32 @@
 
 TEST(AssetResolverTester, CheckPathExists)
 {
+  std::string true_path = hwd::tests::get_asset_dir() + "eve.urdf";
+
   std::string mesh_path = hws::AssetResolver::instance().getFullPath("package://hello_worlds/assets/eve.urdf");
-  EXPECT_EQ(mesh_path, "");
+  if(const char* ament_ptr = std::getenv("AMENT_PREFIX_PATH"))
+  {
+    EXPECT_NE(mesh_path, "");
+    EXPECT_NE(mesh_path, true_path); // In ROS2 the path should be found but should not point to the source asset path
+  }
+  else
+    EXPECT_EQ(mesh_path, "");
 
   hws::AssetResolver::instance().registerPackage("hello_worlds", hwd::tests::get_package_dir());
 
   mesh_path = hws::AssetResolver::instance().getFullPath("package://hello_worlds/assets/eve.urdf");
-  std::string true_path = hwd::tests::get_asset_dir() + "eve.urdf";
   EXPECT_EQ(mesh_path, true_path);
 
   hws::AssetResolver::instance().clear();
 
   mesh_path = hws::AssetResolver::instance().getFullPath("package://hello_worlds/assets/eve.urdf");
-  EXPECT_EQ(mesh_path, "");
+  if(const char* ament_ptr = std::getenv("AMENT_PREFIX_PATH"))
+  {
+    EXPECT_NE(mesh_path, "");
+    EXPECT_NE(mesh_path, true_path); // In ROS2 the path should be found but should not point to the source asset path
+  }
+  else
+    EXPECT_EQ(mesh_path, "");
 
   hws::AssetResolver::instance().addSearchDirectory(hwd::tests::get_package_dir());
 
